ab4fd9c0593c429ba8712096de1cf26421193fd1
Gabriel Pedroza <77304345+GabrielPedroza@users.noreply.github.com>
2026-05-03 14:39:32 -0400

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,24 @@
+class TimeMap:
+
+    def __init__(self):
+        self.time_map = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.time_map[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        value = ""
+
+        l, r = 0, len(self.time_map[key]) - 1
+
+        while l <= r:
+            curr_index = l + (r - l) // 2
+            curr_value, curr_timestamp = self.time_map[key][curr_index]
+
+            if curr_timestamp <= timestamp:
+                value = curr_value
+                l = curr_index + 1
+            else:
+                r = curr_index - 1
+
+        return value
